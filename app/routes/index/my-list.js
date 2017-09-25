f3a7a3d1605dee1ca64407e9a17b0009c8c8f5a6
Marcus Renno <renno.marcus@gmail.com>
2017-09-24 21:59:54 -0400

    Added mirage. Model now is fetched using ember-data.

--- app/routes/index/my-list.js
+++ app/routes/index/my-list.js
@@ -2,11 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let songs = [
-      {'id': 1, 'title':'Sultans of Swing'},
-      {'id': 2, 'title': 'Stairway To Heaven'},
-      {'id': 3, 'title': 'Time'}
-    ];
-    return songs;
+    return this.get('store').findAll('song');
   }
 });
