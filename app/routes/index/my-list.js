5d129b30818ca67ac53b3c66efd8db7ef77bb6e5
Marcus Renno <renno.marcus@gmail.com>
2017-09-24 21:26:14 -0400

    Added component card. Added hard coded model.

--- app/routes/index/my-list.js
+++ app/routes/index/my-list.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    let songs = [
+      {'id': 1, 'title':'Sultans of Swing'},
+      {'id': 2, 'title': 'Stairway To Heaven'},
+      {'id': 3, 'title': 'Time'}
+    ];
+    return songs;
+  }
 });
