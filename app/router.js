fbdf188b6c74b2a1bf416f10bc34bfce1b7e5f6d
Marcus Renno <renno.marcus@gmail.com>
2017-09-24 20:42:54 -0400

    Added routes

--- app/router.js
+++ app/router.js
@@ -7,6 +7,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('index.my-list');
+
+  this.route('index', function() {
+    this.route('my-list');
+  });
 });
 
 export default Router;
